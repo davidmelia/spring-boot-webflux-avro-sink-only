6be716a9ab8cb434efac526fc0a08245f238c21e
meliad2 <david.melia@ii.co.uk>
2021-12-13 15:51:09 +0000

    bug

--- src/main/java/example/StaticInstrumentEventConsumer.java
+++ src/main/java/example/StaticInstrumentEventConsumer.java
@@ -6,25 +6,15 @@ import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import uk.co.dave.consumer.fxrate.consumer.avro.AvroFxRateEvent;
 
 @Component
 public class StaticInstrumentEventConsumer {
 
-
-
-  // @Bean
-  // public Consumer<Flux<Message<AvroFxRateEvent>>> fxRates() {
-  // return events -> events.subscribe(event -> {
-  // System.out.println(event);
-  // });
-  // }
-
-
-
   @Bean
-  public Function<Flux<Message<Object>>, Mono<Void>> fxRates() {
-    return events -> events.flatMapSequential(eventList -> {
-      System.out.println("***" + eventList + "***");
+  public Function<Flux<Message<AvroFxRateEvent>>, Mono<Void>> fxRates() {
+    return events -> events.flatMapSequential(event -> {
+      System.out.println("***" + event + "***");
       return Mono.empty();
     }, 1).then();
   }
