2e46920f0201a5272abb8d415efeae7500dd93ca
meliad2 <david.melia@ii.co.uk>
2021-12-13 15:38:16 +0000

    better

--- src/main/java/example/MyApplication.java
+++ src/main/java/example/MyApplication.java
@@ -2,10 +2,6 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class MyApplication {
@@ -14,13 +10,13 @@ public class MyApplication {
     SpringApplication.run(MyApplication.class, args); //
   }
 
-  @Bean
-  @Order(value = Ordered.LOWEST_PRECEDENCE)
-  public NettyServerCustomizer nettyServerWarmer() {
-    return httpServer -> {
-      System.out.println("My custom code");
-      return httpServer;
-    };
-  }
+  // @Bean
+  // @Order(value = Ordered.LOWEST_PRECEDENCE)
+  // public NettyServerCustomizer nettyServerWarmer() {
+  // return httpServer -> {
+  // System.out.println("My custom code");
+  // return httpServer;
+  // };
+  // }
 
 }
--- src/main/java/example/StaticInstrumentEventConsumer.java
+++ src/main/java/example/StaticInstrumentEventConsumer.java
@@ -1,6 +1,5 @@
-package uk.co.dave.consumer.fxrate.consumer.avro;
+package example;
 
-import java.util.List;
 import java.util.function.Function;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
@@ -12,11 +11,20 @@ import reactor.core.publisher.Mono;
 public class StaticInstrumentEventConsumer {
 
 
+
+  // @Bean
+  // public Consumer<Flux<Message<AvroFxRateEvent>>> fxRates() {
+  // return events -> events.subscribe(event -> {
+  // System.out.println(event);
+  // });
+  // }
+
+
+
   @Bean
-  public Function<Flux<Message<Object>>, Mono<Void>> test() {
+  public Function<Flux<Message<Object>>, Mono<Void>> fxRates() {
     return events -> events.flatMapSequential(eventList -> {
-      List<Object> eventsList = (List<Object>) eventList.getPayload();
-      System.out.println(eventList);
+      System.out.println("***" + eventList + "***");
       return Mono.empty();
     }, 1).then();
   }
